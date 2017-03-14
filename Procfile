9d6c02a56166899d1746014934f14f40163cca49
Jan Van Den Broeck <jvandenbroeck@salesforce.com>
2017-03-14 21:25:54 +0100

    Add flask skeleton

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
+web: gunicorn web:app
 worker_1: python producer.py
 worker_consumer: python consumer.py
---
+++ web.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+app.producer = kafka_helper.get_kafka_producer()
+
+@app.route('/tx', method=['POST'])
+def post_tx():
+    parsed_json = request.get_json()
+    print(len(parsed_json))
+
+    print('LicensePlate = {}'.format(parsed_json[0]["LicensePlate"]))
+
+    return "OK"
