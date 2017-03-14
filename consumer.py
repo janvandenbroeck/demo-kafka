8610bcc1a01c73c550eec2f3ab0a620db237dfca
Jan Van Den Broeck <jvandenbroeck@salesforce.com>
2017-03-14 22:02:14 +0100

    add debug in consumer

--- consumer.py
+++ consumer.py
@@ -7,3 +7,5 @@ consumer = kafka_helper.get_kafka_consumer(topic='truck-action')
 
 for message in consumer:
     print(message)
+    json_record = message.value
+    print(json_record['LicensePlate'])
