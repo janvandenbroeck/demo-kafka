a304f6db7c50f5d4e90b5f5c94ad7e2a608f7907
Jan Van Den Broeck <jvandenbroeck@salesforce.com>
2017-03-15 14:25:59 +0100

    fix column case

--- consumer.py
+++ consumer.py
@@ -20,8 +20,8 @@ for message in consumer:
         pass
 
     print(result)
-    fuel = result["Fuel__c"] + json_record["FuelConsumed"]
-    mileage = result["Mileage__c"] + json_record['MileageDriven']
+    fuel = result["fuel__c"] + json_record["FuelConsumed"]
+    mileage = result["mileage__c"] + json_record['MileageDriven']
 
     average_consumption_l_100km = (100/mileage) * fuel
 
