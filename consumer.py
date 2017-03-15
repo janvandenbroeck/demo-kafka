d99cbd8d5c3c67706b2301393878889d91e70bc2
Jan Van Den Broeck <jvandenbroeck@salesforce.com>
2017-03-15 18:13:41 +0100

    fix Bug

--- consumer.py
+++ consumer.py
@@ -24,7 +24,7 @@ def update_fuelconsumption(licenseplate, km, fuel):
 
     if row_count == 0:
         print("License plate {} Not Found: Creating".format(licenseplate))
-        ins = trucks.insert().values(licenseplate__c=licenseplate, phone__c="+32491623693")
+        ins = trucks.insert().values(licenseplate__c=licenseplate, phone__c="+32491623693", fuel__c=0, mileage__c=0)
         conn.execute(ins)
 
     result = conn.execute("SELECT licenseplate__c, fuel__c, mileage__c, phone__c FROM salesforce.truck__c WHERE licenseplate__c = '{}' LIMIT 1".format(licenseplate)).first()
