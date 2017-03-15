d07bc67250c67a90fd0de5e598ec962b27d46414
Jan Van Den Broeck <jvandenbroeck@salesforce.com>
2017-03-15 14:54:53 +0100

    add mongo

--- consumer.py
+++ consumer.py
@@ -1,14 +1,22 @@
 import kafka_helper
 from sqlalchemy import create_engine, Table, MetaData
+from pymongo import MongoClient
 import logging
 import os
 
+# Postgres setup
 consumer = kafka_helper.get_kafka_consumer(topic='truck-action')
 db_engine = create_engine(os.environ['DATABASE_URL'])
 metadata = MetaData(bind=db_engine)
 trucks = Table('truck__c', metadata, autoload=True, schema='salesforce')
 
+# Mongo setup
+client = MongoClient(os.environ['MONGODB_URI'])
+mongo_db = client.database_names()[0]
+
 def update_fuelconsumption(licenseplate, km, fuel):
+
+    # getting the data
     conn = db_engine.connect()
     result = conn.execute("SELECT licenseplate__c, fuel__c, mileage__c FROM salesforce.truck__c WHERE licenseplate__c = '{}' LIMIT 1".format(licenseplate))
     result = result.first()
@@ -16,21 +24,26 @@ def update_fuelconsumption(licenseplate, km, fuel):
     if not result:
         pass
 
-    print(result)
+    # Calculating the new values
     fuel = result["fuel__c"] + fuel
     mileage = result["mileage__c"] + km
-
     average_consumption_l_100km = (100/mileage) * fuel
 
+    # Updating the record
     result = conn.execute("UPDATE salesforce.truck__c SET average_consumption__c = {}, fuel__c = {}, mileage__c = {} WHERE licenseplate__c = '{}'".format(average_consumption_l_100km, fuel, mileage, licenseplate))
+    conn.close()
 
     print("New Average Fuel Consumption {}".format(average_consumption_l_100km))
 
-    conn.close()
-
+def write_to_mongo(json_msg):
+    truck_actions = mongo_db.truck_actions
+    inserted_id = truck_actions.insert_one(json_msg).inserted_id
+    print(inserted_id)
 
 for message in consumer:
     print(message)
     json_record = message.value
 
     update_fuelconsumption(json_record["LicensePlate"], json_record['MileageDriven'], json_record["FuelConsumed"])
+    write_to_mongo(json_record)
+    
